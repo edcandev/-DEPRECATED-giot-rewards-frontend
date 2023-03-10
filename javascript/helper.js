b72647591a3647dfe306e0f38a3a215a7c507c63
edcan-dev <israel.da.queenie@gmail.com>
2023-03-10 00:48:10 -0600

    avances de noche

--- javascript/helper.js
+++ javascript/helper.js
@@ -6,21 +6,30 @@ const inputSubmitIdentifier = document.querySelector(".input_submit-identifier")
 
 const inputSubmitPrev = document.querySelector(".input_submit-prev");
 
-let data = 
-{
-    "identifier":201920732
-}
-inputSubmitIdentifier.addEventListener("click",(e)=> {
-    /* 
-    fetch("http://192.168.100.89:8080/login",
+
+
+inputSubmitIdentifier.addEventListener("click", async (e)=> {
+
+    
+    // Seleccionar el input y leer valores
+    const inputLogin = document.querySelector(".input_login");
+    
+    let body = JSON.stringify(
+    {
+        "identifier":parseInt(inputLogin.value)
+    })
+
+    let response = await fetch("http://localhost:8080/login",
     {
         method:"POST",
-        body: JSON.stringify(data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    }).then(response => response.json())
-    .then(data => console.log(data)) */
-    let hasPassword
-    console.info("test01_passed")
+        body: body,
+        headers: {"Content-type": "application/json;"}
+    })
+    
+    let data = await response.json();
+
+    console.info(data.hasCredential)
+
     components.showPasswordComponent(false);
 })
 
